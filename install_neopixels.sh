0696967449631dd751e819f47aeff38fe31e7414
armysolo <dhusolo@gmail.com>
2022-01-20 18:22:42 -0600

    Update install_neopixels.sh

--- install_neopixels.sh
+++ install_neopixels.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
+
+run() {
+  $*
+  if [ $? -ne 0 ]
+  then
+    echo "$* failed with exit code $?"
+    return 1
+  else
+    return 0
+  fi
+}
+
 cd ~/
-git clone https://github.com/dreamerwise/NeopixelsKlipper.git
+echo "Cloning repository"
+run git clone https://github.com/dreamerwise/NeopixelsKlipper.git || exit 1
 mv ./NeopixelsKlipper/* ./
 rm -rf NeopixelsKlipper
 cd neopixels
-sudo mv ./klipper_monitor.service /etc/systemd/system/klipper_monitor.service
-systemctl enable klipper_monitor
-systemctl start klipper_monitor
+echo "Setting up service"
+run sudo mv ./klipper_monitor.service /etc/systemd/system/klipper_monitor.service || exit 1
+run systemctl enable klipper_monitor || exit 1
+run systemctl start klipper_monitor || exit 1
+echo "success"
+exit 0
