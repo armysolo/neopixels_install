4aeffe5004feb148c890a626f60091a21f8b9a24
armysolo <dhusolo@gmail.com>
2022-04-06 18:08:32 -0500

    Update neopixel_step_1.sh

--- neopixel_step_1.sh
+++ neopixel_step_1.sh
@@ -17,7 +17,7 @@ run sudo apt-get install python3-pip -y || exit 1
 echo "installing setuptools"
 run sudo pip3 install --upgrade setuptools || exit 1
 echo "installing adafruit libraries"
-run sudo pip3 install --upgrade adafruitpython -shell || exit 1
+run sudo pip3 install --upgrade adafruit-python-shell || exit 1
 echo "installing RPI libraries"
 run wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py || exit 1
 echo "installing raspi blinka"
